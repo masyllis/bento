df0e00f277c201f6c469889dc286652ec368e112
David <>
2023-03-18 20:55:23 -0400

    Ensure VS is installed before workloads and tools

--- packer_templates/scripts/windows/install_software.ps1
+++ packer_templates/scripts/windows/install_software.ps1
@@ -2,8 +2,7 @@
 (new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1', 'C:\Windows\Temp\install.ps1')
 
 $env:chocolateyUseWindowsCompression = 'false'
-for($try = 0; $try -lt 5; $try++)
-{
+for ($try = 0; $try -lt 5; $try++) {
   & C:/Windows/Temp/install.ps1
   if ($?) { break }
   if (Test-Path C:\ProgramData\chocolatey) { break }
@@ -16,23 +15,28 @@ wsl --install --no-launch
 
 # Install with chocolatey
 choco install -y `
-    7zip.install `
-    cmake.install `
-    docker-desktop `
-    firefox `
-    git `
-    googlechrome `
-    neovim `
-    ninja `
-    notepadplusplus.install `
-    python311 `
-    python38 `
-    ruby.install `
-    vim `
-    visualstudio2022professional `
-    vscode.install
+  7zip.install `
+  cmake.install `
+  docker-desktop `
+  firefox `
+  git `
+  googlechrome `
+  neovim `
+  ninja `
+  notepadplusplus.install `
+  python311 `
+  python38 `
+  ruby.install `
+  vim `
+  vscode.install
 
 choco install -y `
-    visualstudio2022-workload-nativecrossplat `
-    visualstudio2022-workload-nativedesktop `
-    visualstudio2022buildtools
+  visualstudio2022professional
+choco install -y `
+  visualstudio2022buildtools `
+  visualstudio2022-remotetools `
+  visualstudio2022-workload-nativecrossplat `
+  visualstudio2022-workload-nativedesktop
+choco install -y visualstudio2022-workload-nativedesktop `
+  --package-parameters `
+  "--productId Microsoft.VisualStudio.Product.Professional --channelId VisualStudio.17.Release --includeRecommended --includeOptional"
